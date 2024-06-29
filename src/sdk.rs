645bec2c0b378ea0beee2885cf3b1b654f1d3a15
Maximilian Schneider <mail@maximilianschneider.net>
2024-06-29 19:51:29 +0100

    Revert "implement if for account shared data (#6)"
    This reverts commit 09f4f727c8b2affc746607a10706761e55caed1b.

--- src/sdk.rs
+++ src/sdk.rs
@@ -1,6 +1,6 @@
 use derive_more::{AsMut, AsRef, Deref, DerefMut, From, Into};
 use solana_program::{pubkey::Pubkey, stake_history::Epoch};
-use solana_sdk::account::{Account, AccountSharedData};
+use solana_sdk::account::Account;
 
 use crate::{
     keyed::Keyed, ReadonlyAccountData, ReadonlyAccountIsExecutable, ReadonlyAccountLamports,
@@ -43,39 +43,6 @@ impl ReadonlyAccountIsExecutable for Account {
 impl ReadonlyAccountRentEpoch for Account {
     fn rent_epoch(&self) -> Epoch {
         self.rent_epoch
-    }
-}
-
-impl ReadonlyAccountLamports for AccountSharedData {
-    fn lamports(&self) -> u64 {
-        AccountSharedData::lamports(self)
-    }
-}
-
-impl ReadonlyAccountData for AccountSharedData {
-    type SliceDeref<'s> = &'s [u8] where Self: 's;
-    type DataDeref<'d> = AccountDataRef<'d> where Self: 'd;
-
-    fn data(&self) -> Self::DataDeref<'_> {
-        AccountDataRef(&AccountSharedData::data(self))
-    }
-}
-
-impl ReadonlyAccountOwner for AccountSharedData {
-    fn owner(&self) -> &Pubkey {
-        AccountSharedData::owner(self)
-    }
-}
-
-impl ReadonlyAccountIsExecutable for AccountSharedData {
-    fn executable(&self) -> bool {
-        AccountSharedData::executable(self)
-    }
-}
-
-impl ReadonlyAccountRentEpoch for AccountSharedData {
-    fn rent_epoch(&self) -> Epoch {
-        AccountSharedData::rent_epoch(self)
     }
 }
 
